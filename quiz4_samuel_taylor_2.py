d66446194ac6879bcf710686daf6ce441a69f535
Sam-Taylor-UIOWA <116521490+Sam-Taylor-UIOWA@users.noreply.github.com>
2024-02-24 08:55:49 -0600

    Add files via upload

---
+++ quiz4_samuel_taylor_2.py
@@ -0,0 +1,161 @@
+######################################################################
+# Edit the following function definition, replacing the words
+# 'name' with your name and 'hawkid' with your hawkid.
+#
+# Note: Your hawkid is the login name you use to access ICON, and NOT
+# your firstname-lastname@uiowa.edu email address.
+#
+# def hawkid():
+#     return(["Caglar Koylu", "ckoylu"])
+######################################################################
+def hawkid():
+    return(["Samuel Zebarth", "skzebarth"])
+
+######################################################################
+# DON'T MAKE ANY CHANGES HERE.
+if (not hawkid()[0] or not hawkid()[1]) or (hawkid()[1] == "hawkid"):
+    print('### Error: YOU MUST provide your hawkid in the hawkid() function.')
+    print('### Otherwise, the Autograder will assign 0 points.')
+    exit(-1)
+else:
+    info = hawkid()
+    print(info[0])
+    print(info[1])
+######################################################################
+
+######################################################################
+# Problem 1 - Getting Input - (40 Points)
+#
+# Specification: You are expected to ask for three main inputs:
+#     1. mode: This input will be a string given by the user to determine
+#              the calculation method. Valid inputs are 'min', 'max', 'avg'.
+#     2. count: This input will be an integer given by the user to indicate
+#               how many more inputs/values are expected to be given. These
+#               new values will be added to a list to be used later in the
+#               calculations.
+#     3. values: These inputs will be floats. You are expected to read user
+#                input 'count' times to get all the values. All of the values
+#                should be stored in a list to be used later.
+#
+# What is expected?
+#     After you get all the inputs from the user, print the following information
+#     in order:
+#           - Print 'mode' variable value (1 Point)
+#           - Print 'count' variable value (1 Point)
+#           - Print length of the list you have just created (1 Point)
+#           - Print the list you have just created (5 Points)
+#     All of the prints should be in separate print statements. Please, don't
+#     forget that the order is very important.
+#
+# Note: Don't forget that input() function returns a string, not a number
+#       even though you give a number as an input. Therefore, be careful
+#       about data type conversions.
+#
+# Example Input/Output:
+#     Assume that you are given the following inputs:
+#       avg
+#       4
+#       123
+#       14234
+#       25435
+#       14
+#
+#     Your solution for Problem 1 should print the following:
+#       Caglar Koylu (Your name will be here)
+#       ckoylu (Your hawkid will be here)
+#       avg
+#       4
+#       4
+#       [123.0, 14234.0, 25435.0, 14.0]
+######################################################################
+
+
+# WRITE YOUR SOLUTION FOR PROBLEM 1 HERE.
+
+p1_list = []
+
+#
+
+p1_input_mode = input("Input your choice (min, max, avg): ")
+
+#  
+
+p1_input_count = int(input('Enter amount of values to be assessed: '))
+
+#
+
+for val in range(p1_input_count):
+    p1_input_val = input('Enter your value to be assessed: ')
+    p1_list.append(float(p1_input_val))
+
+print(p1_input_mode)
+print(p1_input_count)
+print(len(p1_list))
+print(p1_list)
+
+
+
+
+######################################################################
+# Problem 2 - Find the Minimum/Maximum/Average - (60 Points)
+#
+# Specification: In this part of the homework, you are expected to compute
+# a value for the given 'mode'. Therefore, your code has to have three main
+# code blocks:
+#     1. Finding Minimum: If the value of 'mode' is 'min', find the minimum
+#                         element in the list. Then, print its value.
+#     2. Finding Maximum: If the value of 'mode' is 'max', find the maximum
+#                         element in the list. Then, print its value.
+#     3. Finding Average: If the value of 'mode' is 'avg', compute the average
+#                         of all elements in the list. Computation is basically:
+#                               (Sum of all elements)/(Number of elements)
+#
+# Note: BE CAREFUL. YOU CANNOT USE ANY LIBRARIES OR PYTHON'S BUILT-IN
+#       min, max, sum operations. You are expected to implement these operations
+#       by your own. Such an use of those functions are not allowed and may cause
+#       you loose points.
+#
+# Hint: These three code blocks depend on the value of 'mode'. Therefore,
+#       using a conditional branches may help you to perform different
+#       operations in different code blocks. Also, in order to traverse
+#       over each element in the list one by one iteration concepts can
+#       be very helpful.
+#
+# Example Input/Output:
+#     Assume that you are given the following inputs:
+#       avg
+#       4
+#       123
+#       14234
+#       25435
+#       14
+#
+#     Your solution for Problem 1 & 2 should print the following:
+#       Caglar Koylu (Your name will be here)
+#       ckoylu (Your hawkid will be here)
+#       avg
+#       4
+#       4
+#       [123.0, 14234.0, 25435.0, 14.0]
+#       9951.5
+######################################################################
+
+
+# WRITE YOUR SOLUTION FOR PROBLEM 2 HERE.
+
+if p1_input_mode == 'min':
+    p1_list.sort()
+    print(p1_list[0])
+elif p1_input_mode == 'max':
+    p1_list.sort()
+    print(p1_list[-1])
+elif p1_input_mode == 'avg':
+    total = 0
+    for val in p1_list:
+        total += val
+    avg = total / len(p1_list)
+    print(avg)
+else:
+    print('you did not choose an appropriate mode, try again')
+######################################################################
+
